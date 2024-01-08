1c492fcf58bc8babc3d61a56ca760a4c3c574143
Rafid41 <wap.alnahiyan525@gmail.com>
2024-01-09 00:08:38 +0600

    add ingredient dynamically

--- src/Components/BurgerBuilder/Burger/Burger.js
+++ src/Components/BurgerBuilder/Burger/Burger.js
@@ -1,14 +1,37 @@
 // src\Components\BurgerBuilder\Burger\Burger.js
 import React from "react";
 import Ingredient from "../Ingredient/Ingredient";
+import "./Burger.css";
 
 const Burger = (props) => {
+    let ingredientArr = props.ingredients.map((item) => {
+        // proti iteration e amount er count joto hbe, array size toto hbe
+        let amountArr = [...Array(item.amount).keys()];
+
+        // eita diye proti ammountArr k abar full iteration kora
+        return amountArr.map((_) => {
+            //  for(auto _:amountArr) //c++
+            return <Ingredient type={item.type} key={Math.random()} />;
+        });
+    });
+
+    //reduce fn proti array k concat kore 1ta array te convert korbe
+    // eta use kora hoise chk korte j array er total size 0 kina. mane kono ingredient e nai
+    ingredientArr = ingredientArr.reduce((arr, element) => {
+        return arr.concat(element);
+    }, []);
+
+    if (ingredientArr.length === 0) {
+        ingredientArr = <p>Please add some ingrdients!</p>;
+    }
+
+
+
     return (
-        <div>
+        <div className="Burger">
             <Ingredient type="bread-top" />
-            <Ingredient type="cheese" />
-            <Ingredient type="salad" />
-            <Ingredient type="meat" />
+            {/* ingredient type and oto amount print hbe */}
+            {ingredientArr}
             <Ingredient type="bread-bottom" />
         </div>
     );
--- src/Components/BurgerBuilder/BurgerBuilder.js
+++ src/Components/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,21 @@ import React, { Component } from "react";
 import Burger from "./Burger/Burger";
 
 export default class BurgerBuilder extends Component {
+    state = {
+        // ingredients er "type" hbe ingrdients.js er switch-case er case name
+        ingredients: [
+            // amount means ei type koyta count thakbe, amount joto thakbe, totobar call korte hbe
+            //  soja kothay c++: map[type] = amount
+            { type: "salad", amount: 0 },
+            { type: "cheese", amount: 0 },
+            { type: "meat", amount: 0 },
+        ],
+    };
+
     render() {
         return (
             <div>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
             </div>
         );
     }
